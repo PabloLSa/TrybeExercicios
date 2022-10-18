0d2bccdc4de3a959c4cf0a72614168a44d5237c5
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-10-18 13:57:14 -0300

    aletrando váriáveis par const, let e fazendo concatençaões template literals

--- fundamentos/7.1-javaScript-ES6-testesUnitarios/dia-01/script.js
+++ fundamentos/7.1-javaScript-ES6-testesUnitarios/dia-01/script.js
@@ -1,13 +1,14 @@
-function testingScope(escopo) {
+const testingScope = (escopo) => {
   if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
     console.log(ifScope);
   } else {
-    var elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+    const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
     console.log(elseScope);
+    console.log(`${ifScope}  o que estou fazendo aqui ? :O`);
   }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  //console.log(`${ifScope}  o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
 }
 
 testingScope(true);
